e8c94be5c2519a979058c0353fbb23016fd9cf90
Sergey Vinogradov <pettsonchello@gmail.com>
2017-11-10 16:56:54 +0300

    Enable time for flatpickr

--- app/assets/javascripts/main/components/DateControl.js
+++ app/assets/javascripts/main/components/DateControl.js
@@ -3,6 +3,9 @@ import flatpickr from 'flatpickr';
 
 DOMComponent.register('c-DateControl', ($block, props = {}) => {
   flatpickr($block.find('%{c-DateControl/input}')[0], {
-    dateFormat: 'd-m-Y'
+    dateFormat: 'd-m-Y h:i',
+    enableTime: true,
+    minuteIncrement: 10,
+    time_24hr: true
   });
 });
