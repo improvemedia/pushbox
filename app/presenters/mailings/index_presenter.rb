1c6f68978fa29a69a1695d259165f26e36238476
Artem Pyankov <artpyankov@gmail.com>
2017-11-14 16:09:28 +0500

    Show created mailings

--- app/presenters/mailings/index_presenter.rb
+++ app/presenters/mailings/index_presenter.rb
@@ -2,9 +2,33 @@
 
 module Mailings
   class IndexPresenter < ApplicationPresenter
-    def mailing_entities; end
+    class MailingEntity < BaseEntity
+      attribute :id
+      attribute :title
+      attribute :segment_title
+      attribute :dispatch_at
+      attribute :template_title
+      attribute :destroy_url
+    end
+
+    def mailing_entities
+      mailings.map do |mailing|
+        MailingEntity.new(
+          id:             mailing.id,
+          title:          mailing.title,
+          segment_title:  mailing.segment&.title || "Все пользователи",
+          dispatch_at:    mailing.dispatch_at,
+          template_title: mailing.template.title,
+          destroy_url:    r.mailing_path(mailing.id)
+        )
+      end
+    end
 
     private
+
+    def mailings
+      Mailing.includes(:segment, :template)
+    end
 
     def active_page_side_nav_item
       :mailings
